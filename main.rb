eacdd7695fe7e1e000d2fc5382e6503c394931ab
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-30 23:26:09 -0500

    removes most of ruby

--- main.rb
+++ main.rb
@@ -9,32 +9,3 @@ require_relative "models/product_class.rb"
 get "/home" do 
   erb :homepage
 end
-
-post "/add" do 
-  new_product = Product.new({"general_info"=>"#{params["general_info"]}", 
-                            "technical_specs"=>"#{params["technical_specs"]}",
-                            "where_to_buy"=>"#{params["where_to_buy"]}"})
-                           
-                            
-  if new_product.has_errors? == true
-    np_hash = (new_product.errors).to_json # will need to format at some point
-    return np_hash
-  else
-    new_product.insert
-    np_hash = new_product.to_hash
-    np_hash[:worked] = "yes" 
-    return np_hash.to_json
-  end  
-end
-
-get "/view" do 
-  all_products = Product.all
-  all_products.to_json
-end
-
-get "/update" do
-end
-
-post "/delete" do
-  Product.delete(params["id"]) #to_i?
-end
