0bce7a09b05fe6939120d52adebe2536462ae460
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-03-31 09:56:15 -0500

    deletes ruby models/database setup

--- database/database_setup.rb
+++
@@ -1,6 +0,0 @@
-DATABASE = SQLite3::Database.new("/Users/hilarysk/Code/2015-03-08-tabs-database/database/slideshow.db")
-
-DATABASE.results_as_hash = true
-
-DATABASE.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, general_info TEXT, where_to_buy TEXT, technical_specs TEXT)")
-
